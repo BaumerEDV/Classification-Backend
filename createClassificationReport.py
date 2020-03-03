c11c821afcea82c998c98fae4437d77647b2b1ad
Stefan Erwin Baumer <baumeredv@gmail.com>
2020-03-03 20:10:49 +0100

    implemented scaling of the values in the dataframe, which significantly increases accuracy

--- createClassificationReport.py
+++ createClassificationReport.py
@@ -3,6 +3,7 @@ from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
 
 EXPORT_FILE_NAME = "combined_data.csv"
 
@@ -11,6 +12,9 @@ print("min dBm: " + str(min(master_df.min(axis=1))))
 master_df.fillna(-100, inplace=True) #-85 was the best so far?
 classification_target = master_df['nodeId']
 master_df.drop(['Unnamed: 0', 'timestamp', 'nodeId'], axis=1, inplace=True)
+min_max_scaler = preprocessing.MinMaxScaler()
+master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df), columns=master_df.columns, index=master_df.index) #https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
+
 
 X_train, X_test, y_train, y_test = train_test_split(master_df, classification_target, test_size=0.33, stratify=classification_target, random_state=42) #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
