076aa382499933f519d8a6b0236c8c35416db9de
Stefan Erwin Baumer <baumeredv@gmail.com>
2020-03-04 07:05:00 +0100

    added 4-fold cross validation and put output in comments for meeting with Prof. Ludwig

--- createClassificationReport.py
+++ createClassificationReport.py
@@ -5,28 +5,81 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import StratifiedKFold
 
 EXPORT_FILE_NAME = "combined_data.csv"
 
 master_df = pd.read_csv(EXPORT_FILE_NAME)
 print("min dBm: " + str(min(master_df.min(axis=1))))
-master_df.fillna(-100, inplace=True) #-85 was the best so far?
+master_df.fillna(-100, inplace=True) #-85 was the best so far? use -100 though
 classification_target = master_df['nodeId']
 master_df.drop(['Unnamed: 0', 'timestamp', 'nodeId'], axis=1, inplace=True)
 min_max_scaler = preprocessing.MinMaxScaler()
 master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df), columns=master_df.columns, index=master_df.index) #https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
 
-X_train, X_test, y_train, y_test = train_test_split(master_df, classification_target, test_size=0.33, stratify=classification_target, random_state=42) #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+X_train, X_test, y_train, y_test = train_test_split(master_df, classification_target, test_size=0.33, stratify=classification_target, random_state=36) #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
-for classifier in [KNeighborsClassifier(n_neighbors=3), MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42), MultinomialNB()]:
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+classifiers = [
+    {
+        "name": "K(3) Nearest Neighbor",
+        "classifier": KNeighborsClassifier(n_neighbors=3)
+    },
+    {
+        "name": "Feed Forward Neural Network",
+        "classifier": MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42)
+    },
+    {
+        "name": "Multinomial Naive Bayes",
+        "classifier": MultinomialNB()
+    }
+]
+
+for classifier in classifiers:
+    classifier["classifier"].fit(X_train, y_train)
+    y_pred = classifier["classifier"].predict(X_test)
+    print(classifier["name"] + ": " + str(accuracy_score(y_test, y_pred)))
+
+#print(len(y_train))
+
+print("doing 5-fold cross validation now")
+master_df = master_df.to_numpy()
+classification_target = classification_target.to_numpy()
+kf = StratifiedKFold(n_splits=4)
+classifiers = [
+    {
+        "name": "K(3) Nearest Neighbor",
+        "classifier": KNeighborsClassifier(n_neighbors=3)
+    },
+    {
+        "name": "Feed Forward Neural Network",
+        "classifier": MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42)
+    },
+    {
+        "name": "Multinomial Naive Bayes",
+        "classifier": MultinomialNB()
+    }
+]
+for classifier_package in classifiers:
+    accuracies = []
+    classifier = classifier_package["classifier"]
+    for train_index, test_index in kf.split(master_df, classification_target):
+        classifier.fit(master_df[train_index], classification_target[train_index])
+        y_pred = classifier.predict(master_df[test_index])
+        accuracies.append(accuracy_score(y_pred, classification_target[test_index]))
+        #print(len(train_index))
+    print(classifier_package["name"] + ": " + str(accuracies))
 
 
-"""
-classifier = KNeighborsClassifier(n_neighbors=3)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-print(accuracy_score(y_test, y_pred))
-"""
+#output:
+"""""
+min dBm: -97.0
+K(3) Nearest Neighbor: 0.9433962264150944
+Feed Forward Neural Network: 0.9245283018867925
+Multinomial Naive Bayes: 0.7924528301886793
+doing 5-fold cross validation now
+K(3) Nearest Neighbor: [0.875, 0.95, 0.9487179487179487, 0.8974358974358975]
+Feed Forward Neural Network: [0.925, 0.975, 0.9230769230769231, 0.8717948717948718]
+Multinomial Naive Bayes: [0.8, 0.825, 0.7435897435897436, 0.717948717948718]
+
+Process finished with exit code 0
+"""""
