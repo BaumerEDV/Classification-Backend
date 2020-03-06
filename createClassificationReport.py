4b3640b9c7dacbbc4f59bd7f446bab8c6ad50b5d
Stefan Erwin Baumer <Stefan-Erwin.Baumer@stud.uni-regensburg.de>
2020-03-06 15:48:14 +0100

    refactoring

--- createClassificationReport.py
+++ createClassificationReport.py
@@ -6,32 +6,40 @@ from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import StratifiedKFold
+from statistics import median, mean, stdev
+from joblib import dump
 
 EXPORT_FILE_NAME = "combined_data.csv"
 EXPORT_FEATURE_VECTOR_FILE_NAME = "feature_vector_head.csv"
+K_FOLD_NUMBER = 4
 
 
 def save_feature_arry_column_order(feature_array):
-    feature_array[[False] * len(feature_array)].to_csv(EXPORT_FEATURE_VECTOR_FILE_NAME, index=False)
+    feature_array[[False] * len(feature_array)].to_csv(
+        EXPORT_FEATURE_VECTOR_FILE_NAME, index=False)
 
 
 master_df = pd.read_csv(EXPORT_FILE_NAME)
-print("min dBm: " + str(min(master_df.min(axis=1))))
-master_df.fillna(-100, inplace=True) #-85 was the best so far? use -100 though
+master_df.fillna(-100, inplace=True)
 classification_target = master_df['nodeId']
-print(master_df.iloc[0].to_dict())
 master_df.drop(['Unnamed: 0', 'timestamp', 'nodeId'], axis=1, inplace=True)
 min_max_scaler = preprocessing.MinMaxScaler()
-master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df), columns=master_df.columns, index=master_df.index) #https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
+master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df),
+                         columns=master_df.columns,
+                         index=master_df.index)  # https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
 
-X_train, X_test, y_train, y_test = train_test_split(master_df, classification_target, test_size=0.33, stratify=classification_target, random_state=36) #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+X_train, X_test, y_train, y_test = train_test_split(master_df,
+                                                    classification_target,
+                                                    test_size=0.33,
+                                                    stratify=classification_target,
+                                                    random_state=36)  # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 
 save_feature_arry_column_order(X_train)
 
-print("doing 5-fold cross validation now")
+print("doing " + str(K_FOLD_NUMBER) + "-fold cross validation now")
 master_df = master_df.to_numpy()
 classification_target = classification_target.to_numpy()
-kf = StratifiedKFold(n_splits=4)
+kf = StratifiedKFold(n_splits=K_FOLD_NUMBER)
 classifiers = [
     {
         "name": "K(3) Nearest Neighbor",
@@ -39,7 +47,8 @@ classifiers = [
     },
     {
         "name": "Feed Forward Neural Network",
-        "classifier": MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42)
+        "classifier": MLPClassifier(hidden_layer_sizes=140, max_iter=500,
+                                    random_state=42)
     },
     {
         "name": "Multinomial Naive Bayes",
@@ -50,14 +59,15 @@ for classifier_package in classifiers:
     accuracies = []
     classifier = classifier_package["classifier"]
     for train_index, test_index in kf.split(master_df, classification_target):
-        classifier.fit(master_df[train_index], classification_target[train_index])
-        y_pred = classifier.predict(master_df[test_index])
-        accuracies.append(accuracy_score(y_pred, classification_target[test_index]))
-        #print(len(train_index))
-    print(classifier_package["name"] + ": " + str(mean(accuracies)))
+        classifier.fit(master_df[train_index],
+                       classification_target[train_index])
+        y_predictions = classifier.predict(master_df[test_index])
+        accuracies.append(
+            accuracy_score(y_predictions, classification_target[test_index]))
+        # print(len(train_index))
+    print(classifier_package["name"] + ": " + str(median(accuracies)))
 
-
-#output:
+# output:
 """""
 min dBm: -97.0
 K(3) Nearest Neighbor: 0.9433962264150944
@@ -84,13 +94,11 @@ for i in range(len(probabilities)):
         print(classifier.classes_[k] + ": " + str(probability_set[k]))
 """""
 
-#at this point save the best classifier using all the training data; as well as the scaler
-classifier = MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42)
-#classifier = KNeighborsClassifier(n_neighbors=3)
+# at this point save the best classifier using all the training data; as well as the scaler
+classifier = MLPClassifier(hidden_layer_sizes=140, max_iter=500,
+                           random_state=42)
+# classifier = KNeighborsClassifier(n_neighbors=3)
 classifier.fit(master_df, classification_target)
-from joblib import dump
+
 dump(classifier, "classifier.joblib")
 dump(min_max_scaler, "scaler.joblib")
-#print(classifier.predict_proba(master_df[0].reshape(1, -1)))
-#print(classifier.classes_)
-#print(master_df[0])
