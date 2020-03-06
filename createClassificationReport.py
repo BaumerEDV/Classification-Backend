5be839016263d74456acf9328725700e6193bfe9
Stefan Erwin Baumer <Stefan-Erwin.Baumer@stud.uni-regensburg.de>
2020-03-06 22:53:41 +0100

    this pipeline approach is definitely working, might not have to ask after all

--- createClassificationReport.py
+++ createClassificationReport.py
@@ -3,7 +3,8 @@ from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score #https://elitedatascience.com/imbalanced-classes
+from sklearn.metrics import \
+    roc_auc_score  # https://elitedatascience.com/imbalanced-classes
 from sklearn.metrics import f1_score, make_scorer
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
@@ -15,7 +16,8 @@ import scipy.stats as stats
 from sklearn.utils.fixes import loguniform
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.utils import resample
-
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import RandomOverSampler
 
 EXPORT_FILE_NAME = "combined_data.csv"
 EXPORT_FEATURE_VECTOR_FILE_NAME = "feature_vector_head.csv"
@@ -23,7 +25,7 @@ K_FOLD_NUMBER = 4
 RANDOMIZED_SEARCH_ITERATIONS = 30
 
 
-#from: https://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search.html#sphx-glr-auto-examples-model-selection-plot-randomized-search-py
+# from: https://scikit-learn.org/stable/auto_examples/model_selection/plot_randomized_search.html#sphx-glr-auto-examples-model-selection-plot-randomized-search-py
 # Utility function to report best scores
 def report(results, n_top=3):
     for i in range(1, n_top + 1):
@@ -46,13 +48,18 @@ master_df = pd.read_csv(EXPORT_FILE_NAME)
 master_df.fillna(-100, inplace=True)
 master_df.drop(['timestamp', 'Unnamed: 0'], axis=1, inplace=True)
 master_df.drop_duplicates(inplace=True)
-#print(master_df.groupby(master_df.columns.tolist(),as_index=False).size())
+# print(master_df.groupby(master_df.columns.tolist(),as_index=False).size())
 min_max_scaler = preprocessing.MinMaxScaler()
-master_df[master_df.columns.difference(['nodeId'])] = min_max_scaler.fit_transform(master_df[master_df.columns.difference(['nodeId'])])
-train_df, test_df = train_test_split(master_df, test_size=0.33, random_state=36, stratify=master_df['nodeId'])
-print(pd.merge(train_df, test_df, on=train_df.columns.to_list(), how='outer', indicator='Exist')['Exist'].value_counts())
+master_df[
+    master_df.columns.difference(['nodeId'])] = min_max_scaler.fit_transform(
+    master_df[master_df.columns.difference(['nodeId'])])
+train_df, test_df = train_test_split(master_df, test_size=0.33, random_state=36,
+                                     stratify=master_df['nodeId'])
+print(pd.merge(train_df, test_df, on=train_df.columns.to_list(), how='outer',
+               indicator='Exist')['Exist'].value_counts())
 master_df = train_df
-#upsampling
+# upsampling
+"""
 most_common_class = master_df['nodeId'].value_counts().idxmax()
 n_most_common_class = master_df['nodeId'].value_counts().max()
 base_df = master_df.loc[master_df['nodeId'] == most_common_class]
@@ -62,14 +69,17 @@ for class_name in master_df['nodeId'].unique():
         new_df = pd.concat([new_df, base_df], axis=0)
         continue
     n_class = (master_df.nodeId == class_name).sum()
-    class_oversampled_df = master_df.loc[master_df['nodeId'] == class_name].sample(n_most_common_class, replace=True, random_state=42)
+    class_oversampled_df = master_df.loc[
+        master_df['nodeId'] == class_name].sample(n_most_common_class,
+                                                  replace=True, random_state=42)
     new_df = pd.concat([new_df, class_oversampled_df], axis=0)
-    #https://www.kaggle.com/rafjaa/resampling-strategies-for-imbalanced-datasets
+    # https://www.kaggle.com/rafjaa/resampling-strategies-for-imbalanced-datasets
 master_df = new_df
 del new_df
 del base_df
-#print(master_df['nodeId'].unique())
-#end upsampling
+# print(master_df['nodeId'].unique())
+"""
+# end upsampling
 
 """
 #upsampling of test df
@@ -92,16 +102,14 @@ del base_df
 #end upsampling
 """
 
-
-
-#classification_target = master_df['nodeId']
-#master_df.drop(['Unnamed: 0', 'timestamp', 'nodeId'], axis=1, inplace=True)
-#min_max_scaler = preprocessing.MinMaxScaler()
-#master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df),
+# classification_target = master_df['nodeId']
+# master_df.drop(['Unnamed: 0', 'timestamp', 'nodeId'], axis=1, inplace=True)
+# min_max_scaler = preprocessing.MinMaxScaler()
+# master_df = pd.DataFrame(min_max_scaler.fit_transform(master_df),
 #                         columns=master_df.columns,
 #                         index=master_df.index)  # https://stackoverflow.com/questions/26414913/normalize-columns-of-pandas-data-frame
 
-#X_train, X_test, y_train, y_test = train_test_split(master_df,
+# X_train, X_test, y_train, y_test = train_test_split(master_df,
 #                                                    classification_target,
 #                                                    test_size=0.33,
 #                                                    stratify=classification_target,
@@ -111,12 +119,13 @@ y_train = master_df['nodeId']
 X_test = test_df.drop(['nodeId'], axis=1)
 y_test = test_df['nodeId']
 
-#test if any training data is also in the test data
-#print(len(X_train))
-#print(len(X_test))
-#print(X_train.columns.to_list())
-print(pd.merge(X_train, X_test, on=X_train.columns.to_list(), how='outer', indicator='Exist')['Exist'].value_counts())
-#print(set(X_train.columns)==set(X_test.columns))
+# test if any training data is also in the test data
+# print(len(X_train))
+# print(len(X_test))
+# print(X_train.columns.to_list())
+print(pd.merge(X_train, X_test, on=X_train.columns.to_list(), how='outer',
+               indicator='Exist')['Exist'].value_counts())
+# print(set(X_train.columns)==set(X_test.columns))
 
 save_feature_arry_column_order(X_train)
 
@@ -168,15 +177,16 @@ for classifier_package in classifiers:
     accuracies = []
     classifier = classifier_package["classifier"]
     param_dist = classifier_package["param_dist"]
-    random_search = RandomizedSearchCV(classifier, param_distributions=param_dist,
+    random_search = RandomizedSearchCV(classifier,
+                                       param_distributions=param_dist,
                                        n_iter=RANDOMIZED_SEARCH_ITERATIONS,
                                        cv=K_FOLD_NUMBER
-                                       #, scoring=make_scorer(f1_score, average="weighted")
+                                       # , scoring=make_scorer(f1_score, average="weighted")
                                        )
     random_search.fit(X_train, y_train)
     report(random_search.cv_results_)
 
-#output:
+# output:
 """
 Model with rank: 1
 Mean validation score: 0.996 (std: 0.006)
@@ -253,6 +263,55 @@ Parameters: {'fit_prior': True, 'alpha': 0.5}
 
 """
 
+model = Pipeline([
+    ("sampling", RandomOverSampler(random_state=42)),
+    ("classification", MLPClassifier())
+])
+
+param_dist = {
+    "classification__hidden_layer_sizes": [50, 75, 100, 125, 140, 300, 450],
+    "classification__activation": ["identity", "logistic", "tanh", "relu"],
+    "classification__solver": ["lbfgs", "sgd", "adam"],
+    "classification__alpha": [1e-5, 1e-4, 1e-3, 1e-2],
+    "classification__learning_rate": ["constant", "invscaling", "adaptive"],
+    "classification__learning_rate_init": [1e-4, 1e-3, 1e-2],
+    "classification__max_iter": [500, 700, 1000],
+    "classification__shuffle": [False, True],
+    "classification__random_state": [42],
+    "classification__momentum": stats.uniform(0, 1),
+    "classification__nesterovs_momentum": [False, True],
+    "classification__beta_1": [1, 0.9, 0.8, 0.5, 0.1],
+    "classification__beta_2": [0.9999, 0.999, 0.99, 0.9],
+}
+
+random_search = RandomizedSearchCV(model, param_distributions=param_dist,
+                                   n_iter=RANDOMIZED_SEARCH_ITERATIONS,
+                                   cv=K_FOLD_NUMBER
+                                   # , scoring=make_scorer(f1_score, average="weighted")
+                                   )
+random_search.fit(X_train, y_train)
+report(random_search.cv_results_)
+
+"""
+Model with rank: 1
+Mean validation score: 0.929 (std: 0.018)
+Parameters: {'classification__activation': 'tanh', 'classification__alpha': 1e-05, 'classification__beta_1': 0.1, 'classification__beta_2': 0.99, 'classification__hidden_layer_sizes': 450, 'classification__learning_rate': 'invscaling', 'classification__learning_rate_init': 0.001, 'classification__max_iter': 1000, 'classification__momentum': 0.5009070908512535, 'classification__nesterovs_momentum': True, 'classification__random_state': 42, 'classification__shuffle': False, 'classification__solver': 'lbfgs'}
+
+Model with rank: 1
+Mean validation score: 0.929 (std: 0.033)
+Parameters: {'classification__activation': 'tanh', 'classification__alpha': 0.0001, 'classification__beta_1': 0.5, 'classification__beta_2': 0.999, 'classification__hidden_layer_sizes': 125, 'classification__learning_rate': 'adaptive', 'classification__learning_rate_init': 0.001, 'classification__max_iter': 1000, 'classification__momentum': 0.3025932556469719, 'classification__nesterovs_momentum': True, 'classification__random_state': 42, 'classification__shuffle': True, 'classification__solver': 'lbfgs'}
+
+Model with rank: 1
+Mean validation score: 0.929 (std: 0.018)
+Parameters: {'classification__activation': 'identity', 'classification__alpha': 0.0001, 'classification__beta_1': 0.1, 'classification__beta_2': 0.9, 'classification__hidden_layer_sizes': 300, 'classification__learning_rate': 'constant', 'classification__learning_rate_init': 0.01, 'classification__max_iter': 500, 'classification__momentum': 0.8316173902449667, 'classification__nesterovs_momentum': False, 'classification__random_state': 42, 'classification__shuffle': False, 'classification__solver': 'lbfgs'}
+
+Model with rank: 1
+Mean validation score: 0.929 (std: 0.044)
+Parameters: {'classification__activation': 'relu', 'classification__alpha': 0.0001, 'classification__beta_1': 0.8, 'classification__beta_2': 0.999, 'classification__hidden_layer_sizes': 125, 'classification__learning_rate': 'invscaling', 'classification__learning_rate_init': 0.01, 'classification__max_iter': 700, 'classification__momentum': 0.6993644555972881, 'classification__nesterovs_momentum': True, 'classification__random_state': 42, 'classification__shuffle': True, 'classification__solver': 'adam'}
+
+
+"""
+
 
 """""
 classifier = MLPClassifier(hidden_layer_sizes=140, max_iter=500, random_state=42)
@@ -280,32 +339,24 @@ classifier.fit(master_df, classification_target)
 dump(classifier, "classifier.joblib")
 dump(min_max_scaler, "scaler.joblib")
 
-
-
-classifier = MLPClassifier(activation='tanh', alpha=0.001, beta_1=0.5, beta_2=0.9999, hidden_layer_sizes=100, learning_rate='adaptive', learning_rate_init=0.01, max_iter=500, momentum=0.16765559948854347, nesterovs_momentum=True, random_state=42, shuffle=False, solver='adam')
+classifier = MLPClassifier(activation='tanh', alpha=1e-05, beta_1=0.1,beta_2=0.99, hidden_layer_sizes=450, learning_rate='invscaling', learning_rate_init=0.001, max_iter=1000,momentum=0.5009070908512535, nesterovs_momentum=True,random_state=42, shuffle=False, solver='lbfgs')
 classifier.fit(X_train, y_train)
 y_predictions = classifier.predict(X_test)
 print(accuracy_score(y_test, y_predictions))
 y_predictions = classifier.predict_proba(X_test)
-print(roc_auc_score(y_test, y_predictions, multi_class="ovo", average="weighted"))#https://elitedatascience.com/imbalanced-classes
+print(roc_auc_score(y_test, y_predictions, multi_class="ovo",
+                    average="weighted"))  # https://elitedatascience.com/imbalanced-classes
 print(f1_score(y_test, classifier.predict(X_test), average="weighted"))
 print("----")
 
-classifier = MLPClassifier(activation='relu', alpha=0.001, beta_1=0.8, beta_2=0.9999, hidden_layer_sizes=125, learning_rate='constant', learning_rate_init=0.0001, max_iter=1000, momentum=0.1662297780377231, nesterovs_momentum=True, random_state=42, shuffle=False, solver='lbfgs')
+classifier = KNeighborsClassifier(weights='distance', p=1, n_neighbors=4,
+                                  n_jobs=-1, leaf_size=300, algorithm='auto')
 classifier.fit(X_train, y_train)
 y_predictions = classifier.predict(X_test)
 print(accuracy_score(y_test, y_predictions))
 y_predictions = classifier.predict_proba(X_test)
-print(roc_auc_score(y_test, y_predictions, multi_class="ovo", average="weighted"))#https://elitedatascience.com/imbalanced-classes
-print(f1_score(y_test, classifier.predict(X_test), average="weighted"))
-print("----")
-
-classifier = KNeighborsClassifier(weights='distance', p=1, n_neighbors=4, n_jobs=-1, leaf_size=300, algorithm='auto')
-classifier.fit(X_train, y_train)
-y_predictions = classifier.predict(X_test)
-print(accuracy_score(y_test, y_predictions))
-y_predictions = classifier.predict_proba(X_test)
-print(roc_auc_score(y_test, y_predictions, multi_class="ovo", average="weighted"))
+print(
+    roc_auc_score(y_test, y_predictions, multi_class="ovo", average="weighted"))
 print(f1_score(y_test, classifier.predict(X_test), average="weighted"))
 
 classifiers = [
@@ -357,5 +408,6 @@ for classifier_data in classifiers:
     y_predictions = classifier.predict(X_test)
     print(accuracy_score(y_test, y_predictions))
     y_predictions = classifier.predict_proba(X_test)
-    print(roc_auc_score(y_test, y_predictions, multi_class="ovo", average="weighted"))
+    print(roc_auc_score(y_test, y_predictions, multi_class="ovo",
+                        average="weighted"))
     print(f1_score(y_test, classifier.predict(X_test), average="weighted"))
